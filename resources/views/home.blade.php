bca9c3a52ee428ca8617e0e748015064ee2fb691
Jason Droste <jason.droste@hotmail.com>
2024-08-05 18:58:13 -1000

    added blog post functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home</title>
     @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
@@ -16,13 +16,38 @@
         </nav>
         
     @auth
-        <p>You are logged in!</p>
-        <form action="/logout" methdod="POST">
+        <div class="row">
+            <p>You are logged in!</p>
+            <form action="/logout" methdod="POST">
             @csrf
             <button type="submit" class="btn btn-primary">Logout</button>
-        </form>
+            </form>
+        </div>
+        <div class="row">
+            <h3>Create new Post</h3>
+            <form action="/create-post" method="POST">
+                @csrf
+                <input type="text" name="title" placeholder="Post Title"> 
+                <textarea name="body" placeholder="Body content..."></textarea>
+                <button class="btn btn-primary">Submit Post</button>
+            </form>
 
-        
+        </div>
+        <div class="row">
+            <h3>All Posts</h3>
+            @foreach ($posts as $post)
+             <div class="row">
+                <h4>{{$post['title']}} by {{$post->user->name}}</h4> 
+                    {{$post['body']}}  
+                    <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+                    <form action="/delete-post/{{$post->id}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn">Delete</button>
+                    </form> 
+            </div>   
+            @endforeach
+        </div>
     @else   
             <form action="/register" method="POST">
                 @csrf
